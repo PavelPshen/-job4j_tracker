fceca7d5547c9ce481e72e89be6887676f924148
PavelPshen <pshen-leti@mail.ru>
2024-04-07 12:07:43 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String massage;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String massage) {
+        this.active = active;
+        this.status = status;
+        this.massage = massage;
+    }
+
+    public void errorInfo() {
+        System.out.println("Активность: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + massage);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error errorCPU = new Error(false, 34, "CPU error");
+        Error errorGPU = new Error(true, 11, "GPU error");
+        Error errorSSD = new Error(true, 42, "SSD is dead");
+        error.errorInfo();
+        errorCPU.errorInfo();
+        errorGPU.errorInfo();
+        errorSSD.errorInfo();
+    }
+}
