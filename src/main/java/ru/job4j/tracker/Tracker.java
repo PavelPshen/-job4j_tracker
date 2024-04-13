753c7e16c5f3596fe01f5e7c8cb436c3e5002c84
PavelPshen <pshen-leti@mail.ru>
2024-04-13 09:38:34 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,14 +1,61 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Tracker tracker = (Tracker) o;
+        return ids == tracker.ids && size == tracker.size && Arrays.equals(items, tracker.items);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(ids, size);
+        result = 31 * result + Arrays.hashCode(items);
+        return result;
+    }
+
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[items.length];
+        size = 0;
+        for (Item item : items) {
+            if (item != null) {
+                result[size++] = item;
+            }
+        }
+        result = Arrays.copyOf(result, size);
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] resultAll = findAll();
+        Item[] resultName = new Item[resultAll.length];
+        size = 0;
+        for (Item item : resultAll) {
+            if (item.getName().equals(key)) {
+                resultName[size++] = item;
+            }
+        }
+        resultName = Arrays.copyOf(resultName, size);
+        return resultName;
     }
 
     public Item findById(int id) {
