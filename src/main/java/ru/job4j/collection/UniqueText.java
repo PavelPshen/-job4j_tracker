845dee2feaf4a72955d82535376b4188e254f74a
PavelPshen <pshen-leti@mail.ru>
2024-06-05 23:41:18 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean result = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        check.addAll(Arrays.asList(origin));
+        for (String tex : text) {
+            result = check.contains(tex);
+            if (!result) {
+                break;
+            }
+        }
+        return result;
+    }
+}
