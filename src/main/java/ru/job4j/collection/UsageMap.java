4ad4721a32dbc2da412c1633214ea4f644295db3
PavelPshen <pshen-leti@mail.ru>
2024-06-08 09:42:25 +0300

    0. Map, HashMap [#212652 #499856]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,8 +5,18 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+        System.out.println("without matching keys: ");
         map.put("pshen-leti@mail.ru", "Pavel Pshenitsyn");
         map.put("RuPost@yandex.ru", "Ivan Ivanov");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+        map.clear();
+        System.out.println("with matching keys");
+        map.put("pshen-leti@mail.ru", "Pavel Pshenitsyn");
+        map.put("RuPost@yandex.ru", "Ivan Ivanov");
+        map.put("pshen-leti@mail.ru", "Ivan Petrov");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
