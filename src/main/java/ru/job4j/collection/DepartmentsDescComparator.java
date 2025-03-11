a0ddc6ea5e79395b720bcea3e02c2936b5b83c0e
PavelPshen <pshen-leti@mail.ru>
2025-03-11 07:32:52 +0300

    3. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -3,15 +3,13 @@ package ru.job4j.collection;
 import java.util.Comparator;
 
 public class DepartmentsDescComparator implements Comparator<String> {
-    @SuppressWarnings("checkstyle:RightCurly")
     @Override
     public int compare(String left, String right) {
         String[] lefts = left.split("/");
         String[] rights = right.split("/");
-        if (lefts[0].equals(rights[0])) {
+        if (rights[0].compareTo(lefts[0]) == 0) {
             return left.compareTo(right);
-        } else {
-            return right.compareTo(left);
         }
+        return right.compareTo(left);
     }
 }
